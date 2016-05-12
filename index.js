cb6c5a27826c1d2db8a8849dfbd10137ddacea10
Guy Keren <gkeren@expedia.com>
2016-05-12 11:19:08 +0100

    moment tz

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
-var moment = require('moment');
-console.log('London:'+ moment("PST").format("HH:mm"));
+var moment = require('moment-timezone');
+
+var locationToTimezone  = {'LA':'America/Los_Angeles','LONDON':'Europe/London','NY':'America/New_York','BERLIN':'Europe/Berlin'};
+
+for (var zone in locationToTimezone) {
+    console.log(zone + ': ' + moment().tz(locationToTimezone[zone]).format("HH:mm"));
+}
